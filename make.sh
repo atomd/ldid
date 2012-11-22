f2b78b657027024a27aec11574ac9fa094771234
atomd <dwb319@gmail.com>
2012-11-23 03:08:39 +0800

    Add linux support

--- make.sh
+++ make.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
-flags=()
+if [[ `uname` == 'Darwin' ]]; then
 
-sdk=/Developer/SDKs/MacOSX10.4u.sdk
-if [[ -e $sdk ]]; then
-    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
-fi
+    flags=()
 
-for arch in i386 x86_64 ppc armv6; do
-    if g++ -arch "${arch}" --version &>/dev/null; then
-        flags+=(-arch "${arch}")
+    sdk=/Developer/SDKs/MacOSX10.4u.sdk
+    if [[ -e $sdk ]]; then
+        flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
     fi
-done
 
-set -x
-g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+    for arch in i386 x86_64 ppc armv6; do
+        if g++ -arch "${arch}" --version &>/dev/null; then
+            flags+=(-arch "${arch}")
+        fi
+    done
+
+    g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+
+elif [[ `uname` == 'Linux' ]]; then
+    g++ -pipe -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+fi
